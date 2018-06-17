43f49c6341ec2ac38786807a7054b49134ad2176
tgomez <34220115+tgomez@users.noreply.github.com>
2018-06-17 13:21:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
+application.debug = True
 
 # Database Setup
 
-engine = create_engine("sqlite:///collisions.sqlite")
+engine = create_engine("sqlite:///la_collision.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -22,27 +23,26 @@ Base.prepare(engine, reflect=True)
 
 ######### Save references to each table
 
-
 # Create our session (link) from Python to the DB
-session = Session(engine)
+session = session(engine)
 
 # Flask Setup
-
 app = Flask(__name__)
 
 # Flask Routes
-
 @app.route("/")
 def welcome():
-    return (
+    return render_template ('index.html')
         
-    )
+@app.route("/heatmap")
+def heat():
+    return render_template ('heat.html') 
 
-
+@app.route("/timemap")
+def time(): 
+    return render_template ('time.html')
 
 ##
 
-
 if __name__ == '__main__':
     app.run()
-
