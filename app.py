0fcd6d1ebb399d6f03057e28a59321c95deb5560
tgomez <34220115+tgomez@users.noreply.github.com>
2018-06-21 15:20:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,47 @@
-# import dependencies
-import datetime as dt
-import numpy as np
-import pandas as pd
-
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
-
-# Flask Setup
-app = Flask(__name__)
-
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
-
-db = SQLAlchemy(app)
-
+#import dependencies
+from flask import Flask, jsonify, render_template, request, flash, redirect
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, desc,select
+import pandas as pd
+import numpy as np
+import logging
+#################################################
+#resolving Handling issues
+handler = logging.StreamHandler()
+handler.setLevel(logging.ERROR)
+app = Flask(__name__)
+app.logger.addHandler(handler)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
-app.debug = True
-
+#################################################
 # Database Setup
-
-engine = create_engine("sqlite:///db/la_colission.sqlite")
-
+#################################################
+engine = create_engine("sqlite:///la_colission.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
-
-LA_collisions= Base.classes.collision
-
 # reflect the tables
 Base.prepare(engine, reflect=True)
-
-######### Save references to each table
-
 # Create our session (link) from Python to the DB
-#db.session = session(engine)
+session = Session(engine)
 
+#################################################
+# Flask Setup
 
-# Flask Routes
-@app.route("/")
-def welcome():
-    return render_template ('index.html')
-
-#@app.route ("/data", methods=["GET"])
-#def data():
-#    if request.method == "GET":
-#        area_name = request.form["area_name"]
-#        #code to query database
-#        return jsonify(variableUsedToQueryDB)
-
-
-@app.route("/names")
+@app.route('/names')
 def names():
-   # """Return a list of sample names."""
-    stmt = Session.query(LA_collisions).statement
-    df = pd.read_sql_query(stmt, Session.bind)
-    df.set_index('id', inplace=True)
-    print(df)
-    return jsonify(list(df))
+    """Return a list of sample names."""
 
+@app.route("/")
 
-#######
-
-if __name__ == '__main__':
+def main():
+    return render_template('index.html')
+if __name__ == "__main__":
+    app.debug = True
     app.run()
+
+@app.route('/api', methods=['POST', 'GET'])
+def api_response():
+    if request.method == 'POST':
+        return request.json
